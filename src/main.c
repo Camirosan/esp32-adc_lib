f5000a06f4a1905a2f38cc75de5f0428cbe2bb66
Camilo Rojas <greencers@gmail.com>
2022-04-30 13:08:09 +0200

    Primera carga libreria lectura de ADC

---
+++ src/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h> //-
+#include "esp_log.h" //-
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h" //-
+#include "esp_adc_cal.h" //-
+#include "adc_read.h"
+// //Definiciones ADC
+// static const adc_channel_t adc_channel = ADC_CHANNEL_6;
+// static const char *TAG_CH[10] = {"ADC1_CH6"};
+// #define ADC_ATTEN ADC_ATTEN_DB_11;
+// static const adc_bits_width_t adc_width = ADC_WIDTH_BIT_12;
+// static const adc_atten_t adc_attenuation = ADC_ATTEN_DB_0;
+// static const adc_unit_t adc_unit = ADC_UNIT_1;
+
+// void set_up()
+// {
+//     //ad1_config_width
+// }
+
+void app_main() 
+{
+    init_adc();
+
+    uint32_t raw, voltage;
+
+    while(1)
+    {
+        raw = adc_raw();
+        voltage = adv_voltage();
+        printf("-Raw: %d\t-Voltage: %dmV\n", raw, voltage);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
